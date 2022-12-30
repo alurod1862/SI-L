a9fa3bf8277ef784092ff1b2612cd54915d0ae11
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2022-12-30 15:02:49 +0100

    Update usuariosdepartamentos.sh

--- usuariosdepartamentos.sh
+++ usuariosdepartamentos.sh
@@ -1,1 +1,16 @@
-aa
+#!/bin/bash
+
+while read linea; do
+	nombre=$(echo $linea | cut -d ',' -f1)
+        descripcion=$(echo $linea | cut -d ',' -f2)
+        sudo groupadd "$nombre"
+done < departamentos.csv
+
+while read linea; do
+  user=$(echo $linea | cut -d ',' -f1)
+  password=$(echo $linea | cut -d ',' -f2)
+  nombre=$(echo $linea | cut -d ',' -f3)
+  descripcion=$(echo $linea | cut -d ',' -f4)
+  grupo_departamento=$(echo $linea | cut -d ',' -f5)
+  sudo useradd -m -l "$user" -p $(openssl passwd -1 "$password") -c "$nombre" -g "$grupo_departamento"
+done < usuarios.csv
